45668aaafe942e4b9ee46093d7719fa77f9e6f7d
Wuzhou <wuzhou98@hotmail.com>
2020-06-05 17:06:03 +0800

    创建TabBar组件。

---
+++ src/MuiBlazor/Shared/Core/MuiComponentBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace MuiBlazor {
+	public class MuiComponentBase : ComponentBase {
+		protected virtual string CssClass {
+			get {
+				return "";
+			}
+		}
+
+		[Parameter]
+		public virtual string Style {get;set;}
+
+
+	}
+}
